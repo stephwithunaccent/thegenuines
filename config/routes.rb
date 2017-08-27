8c0fe8f5a2baf34b57d9cfaaf2e1cccf1773b84c
arnaudaprahamian <arnaud.aprahamian@gmail.com>
2017-08-27 21:07:12 +0200

    Routes for albums

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :photos, shallow: true do
-    resources :photo_tags, only:[:new, :create]
-    resources :votes, only: [:create]
+  resources :albums, shallow: true do
+    resources :photos do
+      resources :photo_tags, only:[:new, :create]
+      resources :votes, only: [:create]
+    end
   end
 end
