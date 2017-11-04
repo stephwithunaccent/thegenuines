d07ad1c95905cb05a72a2788135dcc5a15eccca2
stephwithunaccent <stephaniecatherine.smith@gmail.com>
2017-11-04 18:33:09 +0100

    fixes dropdown contact

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,14 +2,19 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home, :art, :web, :contact]
 
   def home
+    @albums = Album.all
   end
 
   def art
+    @albums = Album.all
   end
 
+
   def web
+    @albums = Album.all
   end
 
   def contact
+    @albums = Album.all
   end
 end
